62e2654fb687194650bd17153a2268e0708535bb
hawklin2017 <32898629+hawklin2017@users.noreply.github.com>
2026-06-27 22:40:29 +0800

    bugfix

--- internal/api/chat/chat.go
+++ internal/api/chat/chat.go
@@ -248,19 +248,19 @@ func (o *Api) ResetPassword(c *gin.Context) {
 func (o *Api) DelUserAccount(c *gin.Context) {
 	req, err := a2r.ParseRequest[chatpb.DelUserAccountReq](c)
 	if err != nil {
-		log.ZWarn(c, "lintao DelUserAccount parse request failed", err, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount parse request failed", err)
 		apiresp.GinError(c, err)
 		return
 	}
 	opUserID := mctx.GetOpUserID(c)
 	if opUserID == "" {
-		log.ZWarn(c, "lintao DelUserAccount no user id", nil, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount no user id", nil, "req", req)
 		apiresp.GinError(c, errs.ErrNoPermission.WrapMsg("no user id"))
 		return
 	}
 	userType, err := mctx.GetUserType(c)
 	if err != nil {
-		log.ZWarn(c, "lintao DelUserAccount get user type failed", err, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount get user type failed", err, "req", req)
 		apiresp.GinError(c, errs.ErrNoPermission.WrapMsg("missing user type"))
 		return
 	}
@@ -268,7 +268,7 @@ func (o *Api) DelUserAccount(c *gin.Context) {
 		// 普通用户只能删除自己的账号
 		for _, id := range req.UserIDs {
 			if id != opUserID {
-				log.ZWarn(c, "lintao DelUserAccount can only delete own account", nil, "userID", id)
+				log.ZWarn(c, "lintao DelUserAccount can only delete own account", nil, "req", req)
 				apiresp.GinError(c, errs.ErrNoPermission.WrapMsg("can only delete own account"))
 				return
 			}
@@ -278,14 +278,14 @@ func (o *Api) DelUserAccount(c *gin.Context) {
 
 	resp, err := o.chatClient.DelUserAccount(c, req)
 	if err != nil {
-		log.ZWarn(c, "lintao DelUserAccount delete user account failed", err, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount delete user account failed", err, "req", req)
 		apiresp.GinError(c, err)
 		return
 	}
 
 	imToken, err := o.imApiCaller.ImAdminTokenWithDefaultAdmin(c)
 	if err != nil {
-		log.ZWarn(c, "lintao DelUserAccount get IM admin token failed", err, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount get IM admin token failed", err, "req", req)
 		apiresp.GinError(c, err)
 		return
 	}
@@ -294,9 +294,9 @@ func (o *Api) DelUserAccount(c *gin.Context) {
 	//	log.ZWarn(c, "DelUserAccount force offline failed", err, "userID", opUserID)
 	//}
 	if err := o.imApiCaller.DeleteUsers(apiCtx, opUserID); err != nil {
-		log.ZWarn(c, "lintao DelUserAccount delete IM user failed", err, "userID", opUserID)
+		log.ZWarn(c, "lintao DelUserAccount delete IM user failed", err, "req", req)
 	} else {
-		log.ZDebug(c, "lintao DelUserAccount delete IM user success", nil, "userID", opUserID)
+		log.ZDebug(c, "lintao DelUserAccount delete IM user success", nil, "req", req)
 	}
 	apiresp.GinSuccess(c, resp)
 }
