d0233ac3affd920b1e7ed5d73936ee7fcc77be7d
huangke <994906886@qq.com>
2026-07-10 14:46:56 +0800

    修改登录逻辑，增加用户ID登录

--- internal/api/chat/chat.go
+++ internal/api/chat/chat.go
@@ -198,8 +198,9 @@ func (o *Api) RegisterUser(c *gin.Context) {
 }
 
 func (o *Api) Login(c *gin.Context) {
-	//请求参数写入日志
 	req, err := a2r.ParseRequest[chatpb.LoginReq](c)
+	//请求参数写入日志
+	log.ZInfo(c, "LoginReq", "req", req)
 	if err != nil {
 		apiresp.GinError(c, err)
 		return
